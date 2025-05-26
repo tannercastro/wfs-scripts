3eeb0b8a7295221287a2027728ca3c790b7f0c38
tannercastro <tanner.junior@gmail.com>
2025-05-26 12:44:27 -0300

    Update WFSWebClock.js

--- WFSWebClock.js
+++ WFSWebClock.js
@@ -1,35 +1,30 @@
 (function () {
-    
-    
-    
-    
-    const wfsTileAction = 'viewWebClock';
-    const wfsssologinurl = "/sf/idp-init/sso/wfs-sso-login-done?saml2=true&amp;RelayState=True";
 
-    
+    const wfsTileAction = 'viewWebClock';
+    const wfsssologinurl = "/sf/idp-init/sso/wfs-sso-login-done?saml2=true&RelayState=True";
+
     let tileSize = ''.toLowerCase(); 
     let dialogWidth;
 
     switch (tileSize) {
-      case 'sm':
-      case 'small':
-        dialogWidth = '37.5%';
-        break;
-      case 'md':
-      case 'medium':
-        dialogWidth = '50%';
-        break;
-      case 'lg':
-      case 'large':
-        dialogWidth = '64%';
-        break;
-      case 'xl':
-      case 'xlarge':
-        dialogWidth = '80%';
-        break;
-      default:
-        // scale to the size of the tile contents
-        dialogWidth = 'auto';
+        case 'sm':
+        case 'small':
+            dialogWidth = '37.5%';
+            break;
+        case 'md':
+        case 'medium':
+            dialogWidth = '50%';
+            break;
+        case 'lg':
+        case 'large':
+            dialogWidth = '64%';
+            break;
+        case 'xl':
+        case 'xlarge':
+            dialogWidth = '80%';
+            break;
+        default:
+            dialogWidth = 'auto';
     }
 
     const wfsBaseUrl = "https://b3sa.wta-us8.wfs.cloud/workforce/";
@@ -68,57 +63,45 @@
     p.appendChild(wfsCustomTile);
     dialog.appendChild(p);
 
-    // create the dialog footer and add to dialog
-    const footer = document.createElement("footer");
+    // create the dialog footer button and add it directly
     const footerBtn = document.createElement("ui5-button");
-    footer.setAttribute("slot", "footer");
-    footerBtn.addEventListener("click", function() {
-      if (window.jQuery) {
-        $('#wfsCustomDialog').remove();
-      } else {
+    footerBtn.setAttribute("slot", "footer");
+    footerBtn.innerHTML = "Fechar";
+    footerBtn.addEventListener("click", function () {
         dialog.open = false;
-      }
     });
-    footerBtn.innerHTML = "Close";
-    footer.appendChild(footerBtn);
-    dialog.appendChild(footer);
+    dialog.appendChild(footerBtn);
 
-    // add dialog to document.body and then show() it
-    // wfsCustomTile div must exist on screen before running the WFS scripts
+    // add dialog to document.body and open it
     document.body.appendChild(dialog);
     dialog.open = true;
 
-    const serverUnavailableCheck = function() {
-      if (typeof WfsSSOLoginManager === 'undefined') {
-        let style = window.getComputedStyle(document.body);
-        let serverDownMessage = 'The server could not be reached';
-        let messageDiv = document.createElement('div');
-        messageDiv.width = '100%';
-        messageDiv.style.margin = '8px';
-        messageDiv.style.fontFamily = style.fontFamily;
-        messageDiv.style.fontSize = style.fontSize;
-        messageDiv.innerHTML = serverDownMessage;
-        if (dialog) {
-          let sfLoadingContainer = dialog.getElementsByClassName('sfLoadingContainer')[0];
-          dialog.replaceChild(messageDiv, sfLoadingContainer);
+    const serverUnavailableCheck = function () {
+        if (typeof WfsSSOLoginManager === 'undefined') {
+            let style = window.getComputedStyle(document.body);
+            let serverDownMessage = 'The server could not be reached';
+            let messageDiv = document.createElement('div');
+            messageDiv.style.margin = '8px';
+            messageDiv.style.fontFamily = style.fontFamily;
+            messageDiv.style.fontSize = style.fontSize;
+            messageDiv.innerHTML = serverDownMessage;
+            if (dialog) {
+                let sfLoadingContainer = dialog.getElementsByClassName('sfLoadingContainer')[0];
+                dialog.replaceChild(messageDiv, sfLoadingContainer);
+            }
         }
-      }
     };
 
-    // wait in background for 10 seconds, then check if server did not respond
-    setTimeout(serverUnavailableCheck.bind(this), 10000);
+    setTimeout(serverUnavailableCheck, 10000);
 
-    // check for existence of wfsSSOLoginManager script
-    // load it if it does not exist
-      if (!document.getElementById('wfsSSOLoginManager')) {
+    if (!document.getElementById('wfsSSOLoginManager')) {
         let wfsSSOLoginManagerScript = document.createElement('script');
         wfsSSOLoginManagerScript.id = 'wfsSSOLoginManager';
         wfsSSOLoginManagerScript.type = 'text/javascript';
         wfsSSOLoginManagerScript.src = wfsBaseUrl + 'SuccessFactors.do?action=wfsSSOLoginManager';
         document.head.appendChild(wfsSSOLoginManagerScript);
-      } else if (typeof WfsSSOLoginManager !== 'undefined' && WfsSSOLoginManager.isTilePollingComplete()) {
-        // script has already been loaded and WfsSSOLoginManager should exist
+    } else if (typeof WfsSSOLoginManager !== 'undefined' && WfsSSOLoginManager.isTilePollingComplete()) {
         WfsSSOLoginManager.restartTilePolling();
-      }
+    }
 
 })();
